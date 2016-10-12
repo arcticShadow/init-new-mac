a09590f9b2338026a30bd81e9afc6ce56429eb8c
Cole Diffin <cole.diffin@orionhealth.com>
2016-10-12 14:03:14 +1300

    dammit forgot to previously init

---
+++ brew.sh
@@ -0,0 +1,43 @@
+brew install binutils
+brew install diffutils
+brew install ed --with-default-names
+brew install findutils --with-default-names
+brew install gawk
+brew install gnu-indent --with-default-names
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-which --with-default-names
+brew install gnutls
+brew install grep --with-default-names
+brew install gzip
+brew install screen
+brew install watch
+brew install wdiff --with-gettext
+brew install wget
+
+brew install bash
+brew install emacs
+brew install gdb
+brew install gpatch
+brew install m4
+brew install make
+brew install tree
+
+
+brew install file-formula
+brew install git
+brew install less
+brew install openssh
+brew install homebrew/versions/perl518
+brew install python
+brew install rsync
+brew install svn
+brew install unzip
+brew install vim --override-system-vi
+brew install zsh
+
+
+brew install Caskroom/cask/gimp
+brew install Caskroom/cask/java
+brew install Caskroom/cask/charles
+brew install Caskroom/cask/wireshark
