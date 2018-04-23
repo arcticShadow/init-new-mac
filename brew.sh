b5c3dd677e77712fa82d1d7436da337669e523e7
Cole Diffin <cole.diffin@tmnz.co.nz>
2018-04-24 11:33:24 +1200

    converted to Brewfile

--- brew.sh
+++ brew.sh
@@ -1,44 +1,73 @@
-brew install binutils
-brew install diffutils
-brew install ed --with-default-names
-brew install findutils --with-default-names
-brew install gawk
-brew install gnu-indent --with-default-names
-brew install gnu-sed --with-default-names
-brew install gnu-tar --with-default-names
-brew install gnu-which --with-default-names
-brew install gnutls
-brew install grep --with-default-names
-brew install gzip
-brew install screen
-brew install watch
-brew install wdiff --with-gettext
-brew install wget
+#!/bin/bash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install mas
 
-brew install bash
-brew install emacs
-brew install gdb
-brew install gpatch
-brew install m4
-brew install make
-brew install tree
+mkdir /tmp/init-mac
+cd /tmp/init-mac
 
+cat > /tmp/init-mac/Brewfile << EOF
+tap 'caskroom/cask'
+cask_args appdir: "/Applications"
 
-brew install file-formula
-brew install git
-brew install less
-brew install openssh
-brew install homebrew/versions/perl518
-brew install python
-brew install rsync
-brew install svn
-brew install unzip
-brew install vim --override-system-vi
-brew install zsh
+install binutils
+install diffutils
+install ed, args: ["with-default-names"] 
+install findutils, args: ["with-default-names"] 
+install gawk
+install gnu-indent, args: ["with-default-names"] 
+install gnu-sed, args: ["with-default-names"] 
+install gnu-tar, args: ["with-default-names"] 
+install gnu-which, args: ["with-default-names"] 
+install gnutls
+install grep, args: ["with-default-names"] 
+install gzip
+install screen
+install watch
+install wdiff, args: ["with-gettext"] 
+install wget
+
+install bash
+install gdb
+install gpatch
+install m4
+install make
+install tree
 
 
-brew install Caskroom/cask/gimp
-brew install Caskroom/cask/java
-brew install Caskroom/cask/charles
-brew install Caskroom/cask/wireshark
-brew install Caskroom/cask/openshot-video-editor
+install file-formula
+install git
+install less
+install openssh
+install homebrew/versions/perl518
+install python
+install rsync
+install svn
+install unzip
+install vim, args: ["override-system-vi"] 
+install zsh
+
+
+cask gimp
+cask java
+cask charles
+cask wireshark
+cask openshot-video-editor
+cask 'rambox'
+cask 'firefox'
+cask 'google-chrome'
+cask 'opera'
+cask 'spectacle'
+cask 'sequel-pro'
+cask 'vlc'
+cask alfred
+cask bitwarden
+
+mas 'XCode', id: 497799835
+mas 'Pages', id: 409201541
+mas 'Microsoft Remote Desktop', id: 715768417
+mas 'Write', id: 848311469
+mas 'QVR Client', id: 806101067
+EOF
+
+#Runs the installer
+brew bundle
